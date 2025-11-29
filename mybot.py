efa4545c1bbd22164d15d5b6404d3296a820b64c
mahdialin <51255668+mahdialin@users.noreply.github.com>
2025-11-30 01:27:43 +0330

    Add files via upload

---
+++ mybot.py
@@ -0,0 +1,30 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+def start(update: Update, context: CallbackContext):
+    keyboard = [
+        ['گزینه ۱', 'گزینه ۲', 'گزینه ۳'],
+        ['گزینه ۴', 'گزینه ۵']
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    update.message.reply_text("یک گزینه را انتخاب کنید:", reply_markup=reply_markup)
+
+def handle_message(update: Update, context: CallbackContext):
+    text = update.message.text
+    update.message.reply_text(f"شما {text} را انتخاب کردید!")
+
+def main():
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+
+    updater.start_polling()
+    updater.idle()
+
+if _name_ == '_main_':
+    main()
